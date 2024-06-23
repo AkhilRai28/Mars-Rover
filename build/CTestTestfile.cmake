6d919e5c295d6fa7fccb8155dcc5e1923ac85b3d
AkhilRai28 <=>
2024-06-23 22:44:00 +0530

    Added files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/akhil/rover/workspace_one/src
+# Build directory: /home/akhil/rover/workspace_one/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/akhil/rover/workspace_one/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
